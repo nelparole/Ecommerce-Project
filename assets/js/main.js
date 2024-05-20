9d2db991f45390c50fbaa193a2389bacb5b9b97a
nelparole <48744737+nelparole@users.noreply.github.com>
2024-05-20 22:53:59 +1200

    Added new features and fixed bugs
    Added the Newsletter section to provide a means for users to subscribe and receive updates.
    
    Added the footer to enhance navigation and provide essential information about the website.
    
    Fixed various UI bugs related to responsiveness across different viewport sizes. These fixes ensure a consistent and user-friendly experience for users accessing the website on various devices.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,71 @@
 /*=============== SHOW MENU ===============*/
+const navmenu = document.getElementById('nav-menu'),
+        navmenuShop = document.getElementById('nav-toggle'),
+        navmenuClose = document.getElementById('nav-close')
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if (navmenuShop){
+    navmenuShop.addEventListener("click", () =>{
+        navmenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navmenuClose){
+    navmenuClose.addEventListener("click", () =>{
+    navmenu.classList.remove('show-menu')
+})
+}
 
 /*=============== SHOW CART ===============*/
+const cart = document.getElementById('cart'),
+    cartShop = document.getElementById('cart-shop'),
+    cartClose = document.getElementById('cart-close')
+
 
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
+if (cartShop){
+    cartShop.addEventListener("click", () =>{
+        cart.classList.add('show-cart')
+    })
+}
 
 
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
-
+if (cartClose){
+        cartClose.addEventListener("click", () =>{
+        cart.classList.remove('show-cart')
+    })
+}
 
 /*=============== SHOW LOGIN ===============*/
+const login = document.getElementById('login'),
+    loginShop = document.getElementById('login-button'),
+    loginClose = document.getElementById('login-close')
 
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
+if (loginShop){
+    loginShop.addEventListener("click", () =>{
+    login.classList.add('show-login')
+    })
+}
 
 
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
-
+if (loginClose){
+    loginClose.addEventListener("click", () =>{
+    login.classList.remove('show-login')
+})
+}
 
 /*=============== HOME SWIPER ===============*/
 
@@ -97,6 +133,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp() {
+    const scrollup = document.getElementById('scroll-up');
+    // When the scroll is higher than or equal to 350 viewport height, add the 'show-scroll' class to the element with the 'scroll-top' class
+    if (window.scrollY >= 350) {
+        scrollup.classList.add('show-scroll');
+    } else {
+        scrollup.classList.remove('show-scroll');
+    }
+}
+
+// Add scroll event listener to the window, and call the scrollUp function when scrolling occurs
+window.addEventListener('scroll', scrollUp);
 
 
 /*=============== LIGHT BOX ===============*/
